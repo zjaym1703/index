67cf3f84c07a78c0dfb1b54b07a40dc5dedfedcd
zjaym1703 <41943129+zjaym1703@users.noreply.github.com>
2018-10-12 16:16:18 +0900

    Add files via upload

---
+++ tutee_video.php
@@ -0,0 +1,45 @@
+<?php
+require 'vendor/autoload.php';
+
+use OpenTok\OpenTok;
+use OpenTok\MediaMode;
+use OpenTok\ArchiveMode;
+use OpenTok\Session;
+use OpenTok\Role;
+
+$apiKey='46191302';
+$apiSecret='e077924487e0175ec8d5c9344a3dd050c8120470';
+$opentok = new OpenTok($apiKey, $apiSecret);
+
+$conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
+$result=pg_query($conn,"SELECT sessionId FROM VIDEOSESSION where num=1");
+
+while($row=pg_fetch_row($result)){
+  $sessionId=$row[0];
+}
+
+$token = $opentok->generateToken($sessionId);
+/*$token = $token->generateToken(array(
+    'role'       => Role::SUBSCRIBER,
+    'expireTime' => time()+(7 * 24 * 60 * 60), // in one week
+    '*/
+
+// Generate a Token by calling the method on the Session (returned from createSession)
+//$token = $session->generateToken();
+// Set some options in a token
+
+
+$response=array();
+if(isset($sessionId)&&isset($token)){
+  $response["apiKey"]=$apiKey;
+  $response["sessionId"]=$sessionId;
+  $response["token"]=$token;
+}
+
+pg_close($conn);
+
+header('Content-Type: application/json; charset=utf8');
+echo json_encode($response);
+
+
+?>
