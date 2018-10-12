e35d14b825f244e7019495269bf791f32ce0482f
zjaym1703 <41943129+zjaym1703@users.noreply.github.com>
2018-10-12 15:58:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $token = $opentok->generateToken($sessionId);
 $token = $session->generateToken();
 // Set some options in a token
 $token = $session->generateToken(array(
-    'role'       => Role::MODERATOR,
+    'role'       => Role::PUBLISHER,
     'expireTime' => time()+(7 * 24 * 60 * 60), // in one week
     'data'       => 'name=Johnny'//방이름 변경
 ));
@@ -41,8 +41,8 @@ $token = $session->generateToken(array(
 
 //데이터베이스연결
 $conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
-$stat = pg_connection_status($conn);
-echo $stat;
+//$stat = pg_connection_status($conn);
+//echo $stat;
 //pg_query(데이터베이스 연결변수,쿼리);
 pg_query($conn,"insert into(sessionId,roomname,curplay) VIDEOSESSION values('$sessionId','room','true')");
 
