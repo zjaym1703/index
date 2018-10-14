4a8057c655e4edca787de552af7e45dc173da98e
zjaym1703 <41943129+zjaym1703@users.noreply.github.com>
2018-10-14 20:14:22 +0900

    Add files via upload

---
+++ tutor_curplay_change.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+
+$conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
+pg_set_client_encoding($conn, "UTF8");
+
+$sessionId=$_POST["sessionId"];
+
+pg_query($conn,"UPDATE VIDEOSESSION SET curplay=false WHERE sessionId='$sessionId'");
+
+$response=array();
+$response["success"]=true;
+
+pg_close($conn);
+
+echo json_encode($response);
+?>
