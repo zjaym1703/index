ae153930fd72faa60a62d0637a32cfc72cdaf84c
samsandy <42550036+samsandy@users.noreply.github.com>
2018-10-15 20:32:03 +0900

    Update tutee_video.php

--- tutee_video.php
+++ tutee_video.php
@@ -15,7 +15,7 @@ $opentok = new OpenTok($apiKey, $apiSecret);
 $group_num=$_POST["group_num"];
 
 $conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
-$result=pg_query($conn,"SELECT sessionid, roomname FROM VIDEOSESSION where curplay=true");
+$result=pg_query($conn,"SELECT sessionid, roomname FROM VIDEOSESSION where curplay=true and group_num=$group_num");
 echo $result;
 /*group_num=$group_num and */
 while($row=pg_fetch_row($result)){
