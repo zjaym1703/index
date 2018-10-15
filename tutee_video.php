a5257a7b67b41531c6cf8dc92261447f1e9c0c26
samsandy <42550036+samsandy@users.noreply.github.com>
2018-10-15 13:40:08 +0900

    Update tutee_video.php

--- tutee_video.php
+++ tutee_video.php
@@ -15,7 +15,7 @@ $opentok = new OpenTok($apiKey, $apiSecret);
 $group_num=$_POST["group_num"];
 
 $conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
-$result=pg_query($conn,"SELECT sessionid,roomname FROM VIDEOSESSION where curplay=true");
+$result=pg_query($conn,"SELECT sessionid, roomname FROM VIDEOSESSION where curplay=true");
 /*group_num=$group_num and */
 while($row=pg_fetch_row($result)){
   $sessionId=$row[0];
@@ -47,11 +47,11 @@ if(isset($sessionId)&&isset($token)){
   $response["success"]=true;
 }
 
-pg_close($conn);
+//pg_close($conn);
 
 header('Content-Type: application/json; charset=utf8');
 //echo json_encode($response);
 echo json_encode(array("response"=>$response),JSON_UNESCAPED_UNICODE);
 
-//pg_close($conn);
+pg_close($conn);
 ?>
