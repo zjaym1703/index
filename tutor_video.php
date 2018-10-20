3505b3d969901adf9774f3f2413675029db61373
zjaym1703 <41943129+zjaym1703@users.noreply.github.com>
2018-10-21 01:54:17 +0900

    Update tutor_video.php

--- tutor_video.php
+++ tutor_video.php
@@ -11,34 +11,43 @@ $apiKey='46191302';
 $apiSecret='e077924487e0175ec8d5c9344a3dd050c8120470';
 $opentok = new OpenTok($apiKey, $apiSecret);
 
-$conn=pg_connect("host=ec2-23-21-147-71.compute-1.amazonaws.com dbname=dlfs3hk56lv93 user=guysuywytepygg password=cab4905d6f5fcd3034da4bee3305841803936c3953fb18f17cb8082c37a950d1");
-pg_set_client_encoding($conn, "UTF8");
+$conn=mysqli_init();
+//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
+mysqli_real_connect($conn, "appmeet.mysql.database.azure.com", "myadmin@appmeet", "meet2017157920173861!","appmeet", 3306);
+mysqli_set_charset($conn,"utf8");
 
-$video_name=$_POST["video_name"];//fragment 액티비티에서 넘어오는 값
-$group_num=(int)$_POST["group_no"];//마찬가지
+$video_name=$_GET["video_name"];//fragment 액티비티에서 넘어오는 값
+$group_num=(int)$_GET["group_no"];//마찬가지
 
 // Create a session that attempts to use peer-to-peer streaming:
-$session = $opentok->createSession();
+//$session = $opentok->createSession();
 // A session that uses the OpenTok Media Router, which is required for archiving:
-$session = $opentok->createSession(array( 'mediaMode' => MediaMode::ROUTED ));
+//$session = $opentok->createSession(array( 'mediaMode' => MediaMode::ROUTED ));
 //위치 힌트로 사용되는 IPv4 주소입니다. (기본값 : "")
-$session = $opentok->createSession(array( 'location' => '192.168.123.106' ));
+//$session = $opentok->createSession(array( 'location' => '192.168.123.106' ));
 //세션이 자동으로 아카이브
-$sessionOptions = array(
-    'archiveMode' => ArchiveMode::ALWAYS,
-    'mediaMode' => MediaMode::ROUTED
-);
-$session = $opentok->createSession($sessionOptions);
+//$sessionOptions = array(
+//    'archiveMode' => ArchiveMode::ALWAYS,
+ //   'mediaMode' => MediaMode::ROUTED
+//);
+//$session = $opentok->createSession($sessionOptions);
 
+// Store this sessionId in the database for later use
+//$sessionId = $session->getSessionId();
+
+
+//18.10.21 추가
+$session = $opentok->createSession(array('mediaMode' => MediaMode::ROUTED));
+//$SessionOption 대신 mediaMode만 설정
 // Store this sessionId in the database for later use
 $sessionId = $session->getSessionId();
 
 //디비연결
 
 // Generate a Token from just a sessionId (fetched from a database)
-$token = $opentok->generateToken($sessionId);
+//$token = $opentok->generateToken($sessionId);
 // Generate a Token by calling the method on the Session (returned from createSession)
-$token = $session->generateToken();
+//$token = $session->generateToken();
 // Set some options in a token
 $token = $session->generateToken(array(
     'role'       => Role::PUBLISHER,
@@ -55,15 +64,15 @@ $token = $session->generateToken(array(
   } else {
       echo 'Connection status bad';
   }*/
-pg_query($conn,"INSERT INTO VIDEOSESSION(sessionId,roomname,curplay,group_num) VALUES('$sessionId','$video_name',true,'$group_num')");
+//pg_query($conn,"INSERT INTO VIDEOSESSION(sessionId,roomname,curplay,group_num) VALUES('$sessionId','$video_name',true,'$group_num')");
 //group_num추가 --> 테이블 구조도 변경함
 
-$response=array();
-if(isset($sessionId)&&isset($token)){
-  $response["apiKey"]=$apiKey;
-  $response["sessionId"]=$sessionId;
-  $response["token"]=$token;
-}
+//$response=array();
+//if(isset($sessionId)&&isset($token)){
+//  $response["apiKey"]=$apiKey;
+//  $response["sessionId"]=$sessionId;
+//  $response["token"]=$token;
+//}
 
 //세션의 아카이브를 만듦
 /*
@@ -81,8 +90,23 @@ $archive = $opentok->startArchive($sessionId, $archiveOptions);
 // archiveId 디비에 저장
 $archiveId = $archive->id;*/
 
-pg_close($conn);
+$archiveId="";
+$curplay=1;//현재 진행중
+$response= array();
 
-header('Content-Type: application/json; charset=utf8');
+$statement = mysqli_prepare($conn, "INSERT INTO VIDEOSESSION(sessionId,roomname,curplay,archiveId,group_num) VALUES(?,?,?,?,?)");
+mysqli_stmt_bind_param($statement, "ssisi", $sessionId, $video_name, $curplay, $archiveId,$group_num);
+
+$response["success"]=false;
+
+while(mysqli_stmt_fetch($statement)){
+  $response["success"]=true;
+  $response["apiKey"]=$apiKey;
+  $response["sessionId"]=$sessionId;
+  $response["token"]=$token;
+}
+
+mysqli_close($conn);
+
 echo json_encode($response);
 ?>
