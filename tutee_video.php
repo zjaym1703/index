f2a942a8f7f067b2cffe9ea64ef739b240edd0fa
zjaym1703 <41943129+zjaym1703@users.noreply.github.com>
2018-12-11 11:04:23 +0900

    Update tutee_video.php

--- tutee_video.php
+++ tutee_video.php
@@ -9,12 +9,8 @@ use OpenTok\Role;
 $apiKey='46191302';
 $apiSecret='e077924487e0175ec8d5c9344a3dd050c8120470';
 
-//$con=mysqli_init();
-//mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);
-//mysqli_real_connect($con, "appmeet.mysql.database.azure.com", "myadmin@appmeet", "meet2017157920173861!","appmeet", 3306);
-//$con=mysqli_connect("us-cdbr-iron-east-01.cleardb.net","b5abea0f4c48d4","f97c6b56","heroku_82b359327db23c4");
 $conn=mysqli_connect("appmeet.mysql.database.azure.com", "myadmin@appmeet", "meet2017157920173861!","appmeet", 3306);
-//mysqli_real_connect($conn, "appmeet.mysql.database.azure.com", "myadmin@appmeet", "meet2017157920173861!","appmeet", 3306);
+
 mysqli_set_charset($conn,"utf8");
 
 $group_num=$_GET["group_no"];
@@ -31,15 +27,6 @@ while($row=mysqli_fetch_array($state)){
     $videoName=$row[1];
     $token=$row[2];
 }
-
-/*
-if($sessionId){
-  $token=$opentok->generateToken($sessionId, array(
-    'role'       => RoleConstants::PUBLISHER,
-    'expireTime' => time()+(7 * 24 * 60 * 60), // in one week
-    ));
-}*/
-
 
 $response=array();
 $response["success"]=false;
